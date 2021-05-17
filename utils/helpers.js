cd872e390ae9e2ede9905321ac2bf5a60b1b2347
Luis <luis.cabrera@gmail.com>
2021-05-16 19:50:03 -0500

    now capturing post_body and saving it in DB

--- utils/helpers.js
+++ utils/helpers.js
@@ -11,12 +11,7 @@ module.exports = {
 
     return word;
   },
-  format_url: url => {
-    return url
-      .replace('http://', '')
-      .replace('https://', '')
-      .replace('www.', '')
-      .split('/')[0]
-      .split('?')[0];
-  },
+  format_short: post_body => {
+    return post_body.slice(0,100)+`...`;
+  }
 }
